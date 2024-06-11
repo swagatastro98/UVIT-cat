dd6359c229ad720dab91f7c8a020b7a17c2fe4b4
Swagat Bordoloi <swagatbordoloi98@gmail.com>
2024-06-11 08:30:35 +0530

    pre-final draft of folder

--- test_cat.py
+++ test_cat.py
@@ -57,7 +57,7 @@ def test(path):
     # print(pri_hdr.keys)
 
 
-test('/Users/swagat98/Documents/UVIT-cat/UVIT-cat_merged.fits')
+# test('/Users/swagat98/Documents/UVIT-cat/UVIT-cat_merged.fits')
 
 
 def add_columns(path):
@@ -93,3 +93,53 @@ def add_columns(path):
 
     # print(df['N5_M_A'].shape)
 # add_columns('/Users/swagat98/UVIT-cat.fits')
+
+
+
+def find_filterwise_sources(path):
+
+    
+    img = fits.open(path)
+
+    pri_data = img[0].data
+    pri_hdr  = img[0].header
+    bin_data = img[1].data
+    bin_hdr  = img[1].header
+
+    # print(bin_hdr.keys)
+
+    newdf = Table(bin_data)
+    df    = newdf.to_pandas()
+
+    print(f'Total sources in the dataframe: { df.shape }')
+
+    f1_df = df[df['F1_M_A']>0]
+    f2_df = df[df['F2_M_A']>0]
+    f3_df = df[df['F3_M_A']>0]
+    f5_df = df[df['F5_M_A']>0]
+    f7_df = df[df['F7_M_A']>0]
+
+    print(f'F1 length: {f1_df.shape}')
+    print(f'F2 length: {f2_df.shape}')
+    print(f'F3 length: {f3_df.shape}')
+    print(f'F5 length: {f5_df.shape}')
+    print(f'F7 length: {f7_df.shape}')
+
+    n1_df = df[df['N1_M_A']>0]
+    n2_df = df[df['N2_M_A']>0]
+    n3_df = df[df['N3_M_A']>0]
+    n5_df = df[df['N5_M_A']>0]
+    n6_df = df[df['N6_M_A']>0]
+
+    print(f'N1 length: {n1_df.shape}')
+    print(f'N2 length: {n2_df.shape}')
+    print(f'N3 length: {n3_df.shape}')
+    print(f'N5 length: {n5_df.shape}')
+    print(f'N6 length: {n6_df.shape}')
+
+
+
+
+
+
+find_filterwise_sources('/Users/swagat98/Documents/UVIT-cat/UVIT-cat_merged.fits')
